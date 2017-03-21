eb2b4b56826e7bef84b6ddd1d2e2b5a59e2d1023
Jack Jiang <shenghangjiang8715487@gmail.com>
2017-03-21 15:21:36 -0500

    Finished the moves for all the pieces
    It’s now playable
    
    *added movements for all the pieces
    *added moved(variable/function that checks if the piece has already
    been moved) to Piece class
    *added set_piece function to ChessBoard that allows Game class to
    manipulate the moved attribute of the Piece class
    *additional comments

--- Piece.cpp
+++ Piece.cpp
@@ -12,11 +12,17 @@ string Piece::get_name(){
 string Piece::get_team(){
   return Team;
 }
+int Piece::get_moved(){
+  return Moved;
+}
 void Piece::set_name(string name){
   Name = name;
 }
 void Piece::set_team(string team){
   Team = team;
+}
+void Piece::set_moved(int moved){
+  Moved = moved;
 }
 void Piece::set_Next(Piece* next){
   Next = next;
--- Piece.h
+++ Piece.h
@@ -11,9 +11,11 @@ public:
   ~Piece();
   string get_name();
   string get_team();
+  int get_moved(); //see if the piece has moved or not
   void set_name(string name);
   void set_team(string team);
   Piece * get_Next();
+  void set_moved(int moved);
   void set_Next(Piece * next);
   void print_piece_name();
   Coordinate Location;
@@ -22,5 +24,6 @@ public:
 private:
   string Name;
   string Team;
+  int Moved; //0 is hasn't moved 1 is has moved
   Piece *Next;
 };
